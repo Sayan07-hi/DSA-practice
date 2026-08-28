a23710dd327e4211e883954a7b9b36d7245103a0
Sayan07-hi <dassayanjit3@gmail.com>
2026-08-29 00:49:48 +0530

    Added stack in C and Python

--- Stack/C/ArrayStack.c
+++ Stack/C/ArrayStack.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <stdbool.h>
+
 int top=-1, size;
 void push(int stack_arr[],int);
 void pop(int stack_arr[]);
 void peek(int stack_arr[]);
+void display(int stack_arr[]);
 
 int main(){
     int i,choice,val;
@@ -11,8 +14,8 @@ int main(){
     printf("Enter the size of the stack: ");
     scanf("%d",&size);
     int  stack_arr[size];
-    do{
-    printf("Enter your choice: 1.push 2.pop 3.peek\n");
+    while(true){
+    printf("Enter your choice: 1.push 2.pop 3.peek 4.Display 5.Exit\n");
     scanf("%d",&choice);
     switch (choice){
         case 1:
@@ -27,15 +30,27 @@ int main(){
         case 3:
             peek(stack_arr);
             break;
+        case 4:
+            display(stack_arr);
+            break;
+        case 5:
+            return 0;;
         default:
             printf("wrong input.\n");
 
     }
-    printf("y/n?\n");
-    scanf(" %c",&cont);
-    }while(cont=='y');
+    }
     
     return 0;
+}
+void display(int stack_arr[]){
+    if(top==-1)
+    printf("Stack is empty.\n");
+    else
+    for(int i=top;i>=0;i--){
+        printf("%d\n",stack_arr[i]);
+    }
+
 }
 void push(int stack_arr[],int data){
     if(top==size-1)
